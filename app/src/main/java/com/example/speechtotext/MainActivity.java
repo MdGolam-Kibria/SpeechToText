d32f0be8c2ac52265a37c8a307b87831cbfc3492
AgamiSoft <golamkibria.java@gmail.com>
2020-11-10 18:26:48 +0600

    add copy text

--- app/src/main/java/com/example/speechtotext/MainActivity.java
+++ app/src/main/java/com/example/speechtotext/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.speechtotext;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ActivityNotFoundException;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,11 +24,18 @@ public class MainActivity extends AppCompatActivity {
     TextView textView;
     Button startBangla, startEnglish;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inti();
+        textView.setOnClickListener(v -> {//for copy the text
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("PhoneNumber", textView.getText());
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, "Copy", Toast.LENGTH_SHORT).show();
+        });
         startBangla.setOnClickListener(v -> {//for bangla speech to bangla text
 
 
